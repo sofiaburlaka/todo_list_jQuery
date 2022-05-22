b548a787355ddf0754b9b335f1485f69ffe15385
Sofia Burlaka <sofiaburlaka@gmail.com>
2022-05-22 21:02:57 +0300

    List

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    let count = 0;
+    $(".add").click(function(){
+        let inputvalue = $(".form-control").val();
+        if(inputvalue != ''){
+            count++;
+            $(".todo-list").append('<li><div class="left-cont"><input type="checkbox" id="check-'+count+'" name=""><label for="check-'+count+'" class="label"></label><span>'+inputvalue+' </span></div><span class="remove"><i class="material-icons second-icon"> delete </i></span></li>' )
+        }
+        $(".form-control").val(' ');
+    })
+
+    $(document).on('change', 'input[type="checkbox"]', function(){
+        if($(this).is(':checked')){
+            $(this).closest("li").children(".remove").addClass("active");
+        }else{
+            $(this).closest("li").children(".remove").removeClass("active");
+        }
+    })
+    $(document).on('click', '.remove', function(){
+    $(this).parent().remove ();
+
+    })
+})
